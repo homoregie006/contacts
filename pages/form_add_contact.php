57c56507ef720a999c2afc20627e525fc6f863a4
unknown <homoregie006@wit-xdvm-045.MILLARDPS.NET>
2012-11-09 11:10:29 -0600

    Changed form_add_contacts

---
+++ pages/form_add_contact.php
@@ -0,0 +1,28 @@
+<form class ="form-horizontal" action ="actions/add_contact.php" method="post">
+	<div class="control-group">
+		<label class="control-label" for="name">Name</label>
+		<div class="controls">
+			<input name="name" type="text" placeholder="First Name" class="span3" /> <input name="name" type="text" placeholder="Last Name" class="span3" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label" for="name">Email</label>
+		<div class="controls">
+			<input name="name" type="text" placeholder="Email Address" class="span5" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label" for="name">Phone</label>
+		<div class="controls">
+			<input name="name" type="text" placeholder="No Dashes" class="span3" />
+		</div>
+	</div>
+	<div class="form actions">
+		<div class="form-actions">
+ 		<button type="submit" class="btn btn-success">Save changes</button>
+ 		<button type="button" class="btn" onclick="window.history.go(-1)">Cancel</button>
+		</div>
+	</div>
+			
+	
+</form>
